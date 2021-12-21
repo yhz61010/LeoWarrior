a96542e20dcc10347012c80353afef5f8c80facc
Michael Leo <yhzemail61010@aliyun.com>
2021-12-21 10:11:27 +0800

    init

--- core/src/com/leovp/warrior/assets/characters/ModernGuy02.kt
+++ core/src/com/leovp/warrior/assets/characters/ModernGuy02.kt
@@ -1,5 +1,6 @@
 package com.leovp.warrior.assets.characters
 
+import com.badlogic.gdx.math.MathUtils
 import com.leovp.warrior.assets.Assets
 import com.leovp.warrior.assets.characters.base.Character
 
@@ -9,6 +10,25 @@ import com.leovp.warrior.assets.characters.base.Character
  */
 class ModernGuy02(x: Float, y: Float) : Character(x, y, Assets.modernGuy02TextureRegion) {
     override fun getTagName() = TAG
+
+    init {
+        velocity.set(8f, 8f)
+        updateFrameDuration(1f / 6)
+    }
+
+    override fun update(dt: Float) {
+        stateTime += dt
+        val dist = (MathUtils.sin(stateTime) + MathUtils.cos(stateTime)) * velocity.y * dt
+//        Gdx.app.log(TAG, "${MathUtils.sin(stateTime)}")
+
+        status = if (dist >= 0) {
+            Status.RIGHT_WALK
+        } else {
+            Status.LEFT_WALK
+        }
+        position.add(dist, 0f)
+        adjustPosAndUpdate()
+    }
 
     companion object {
         private const val TAG = "Modernguy02"
